0f4af7c6701148381db66743d237bf9e11430998
Jean-Michel POTHELUNE <jm@pothelune.net>
2016-06-30 00:50:26 +0200

    Add MQTT support

--- MiniMailBox_Firmware.ino
+++ MiniMailBox_Firmware.ino
@@ -1,15 +1,59 @@
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include "config.h"
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+const char* mqtt_server_host = "io.adafruit.com";
+const int   mqtt_server_port = 1883;
+
 
 void setup() {
   Serial.begin(115200);
   setup_wifi();
 
+  client.setServer(mqtt_server_host, mqtt_server_port);
+  client.setCallback(callback);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 75, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish(status_topic, msg);
+  }
+  
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // TODO Protocol on message received
 }
 
 
@@ -32,4 +76,26 @@ void setup_wifi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("MiniMailBox", mqtt_server_username, mqtt_server_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(status_topic, "connected");
+      // ... and resubscribe
+      client.subscribe(event_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
